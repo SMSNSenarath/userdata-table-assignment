7faed02ce2bd620cb1f35888401d7515ab126d4d
shaki <smsnshakilaa372@gmail.com>
2022-08-09 21:44:20 +0530

    DB connection established

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,26 @@ const dotenv = require("dotenv");
 //Creating the Express app
 const app = express();
 
+//Importing middlewares to help to parse the incoming request with json payloads
+app.use(express.json());
+
+//Enabling cross origin requests for a REST service
+app.use(cors());
+
+//Config file for environment variable
+dotenv.config();
+
+//Database connection
+mongoose.connect(process.env.MONGO_URL).then(
+    () => {
+        console.log("MongoDB connected")
+    },(err) => {
+        console.log(err)
+    }
+)
+
 //Server initialization
 app.listen(5000, () => {
   console.log("Server started on port 5000");
 });
+
