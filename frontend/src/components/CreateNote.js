7b4b131ce1ca69987fd163e19a93b902f92dd5a8
shaki <smsnshakilaa372@gmail.com>
2022-08-11 19:43:49 +0530

    Added create note service

--- frontend/src/components/CreateNote.js
+++ frontend/src/components/CreateNote.js
@@ -1,6 +1,43 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import axios from "axios";
+
 
 const CreateNote = () => {
+  //Declare states
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const {user} = useContext(AuthContext);
+  const token = {
+    headers: {token: user.accessToken},
+  }
+
+  //Declare Note submission
+  const submitNote = async (e) => {
+    e.preventDefault();
+    const noteDetails = {
+      title: title,
+      description: description
+    }
+    const res = await axios.post(`http://localhost:5000/api/note/create/`,noteDetails, token)
+    .then(()=>{
+      alert("Note created successfully!");
+    })
+    .catch((err)=>{
+      alert(err);
+    })
+   }
+  
+
+  //Declare onChange functions
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+  }
+
+  const onChangeDescription = (e) => {
+    setDescription(e.target.value);
+  }
+
   return (
     <>
       <div class="column is-one-third has-text-left">
@@ -8,19 +45,20 @@ const CreateNote = () => {
         <div class="field">
           <label class="label">Title</label>
           <div class="control">
-            <input class="input is-medium" type="text" />
+            <input class="input is-medium" type="text" value={title} onChange={onChangeTitle} />
           </div>
         </div>
         <div class="field">
           <label class="label">Description</label>
           <div class="control">
-            <textarea class="textarea is-medium"></textarea>
+            <textarea class="textarea is-medium" value={description} onChange={onChangeDescription}></textarea>
           </div>
         </div>
         <div class="control">
           <button
             type="submit"
             class="button is-link is-fullwidth has-text-weight-medium is-medium"
+            onClick={submitNote}
           >
             Create Note
           </button>
@@ -28,6 +66,6 @@ const CreateNote = () => {
       </div>
     </>
   );
-};
+  }
 
 export default CreateNote;
