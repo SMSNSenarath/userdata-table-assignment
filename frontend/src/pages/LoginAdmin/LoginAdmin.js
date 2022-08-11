6f4bb17d1f0095b30ef3809163188ef649010bb9
shaki <smsnshakilaa372@gmail.com>
2022-08-11 22:32:25 +0530

    Added Admin login service

--- frontend/src/pages/LoginAdmin/LoginAdmin.js
+++ frontend/src/pages/LoginAdmin/LoginAdmin.js
@@ -1,48 +1,99 @@
-import React from 'react'
+import React, { useState, useContext } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 const LoginAdmin = () => {
+  //Declare states
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { isFethching, dispatch } = useContext(AuthContext);
+
+  let navigate = useNavigate();
+
+  //Declare Login handling functions
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const loginDetails = {
+      email: email,
+      password: password,
+    };
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/user/login",
+        loginDetails
+      );
+      dispatch({ type: "LOGIN_SUCCESS", payload: response.data });
+      const user = response.data;
+      if(user.accountType == "admin"){
+        navigate("/admin-dashboard");
+      }else{
+        alert("Access denied! You are not an admin!");
+      }
+      
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  //Declare onChange functions
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
   return (
     <>
-     <section class="hero is-fullheight">
-        <div class="hero-body">
-            <div class="container has-text-centered">
-                <div class="column is-4 is-offset-4">
-                    <h3 class="title has-text-black">Login</h3>
-                    <hr class="login-hr"/>
-                    <p class="subtitle has-text-black">Please login to proceed.</p>
-                    <div class="box">
-                        <form>
-                            <div class="field">
-                                <div class="control">
-                                    <input class="input is-large" type="email" placeholder="Your Email" autofocus=""/>
-                                </div>
-                            </div>
- 
-                            <div class="field">
-                                <div class="control">
-                                    <input class="input is-large" type="password" placeholder="Your Password"/>
-                                </div>
-                            </div>
-                            <div class="field">
-                                {/* <label class="checkbox">
-                  <input type="checkbox"/>
-                  Remember me
-                </label> */}
-                            </div>
-                            <button class="button is-block is-danger is-large is-fullwidth is-rounded">Login as Admin</button>
-                        </form>
+      <section className="hero is-fullheight">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <div className="column is-4 is-offset-4">
+              <h3 className="title has-text-black">Login</h3>
+              <hr className="login-hr" />
+              <p className="subtitle has-text-black">
+                Please login to proceed.
+              </p>
+              <div className="box">
+                <form>
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        className="input is-large"
+                        type="email"
+                        placeholder="Your Email"
+                        onChange={onChangeEmail}
+                        value={email}
+                      />
                     </div>
-                    {/* <p class="has-text-grey">
-                        <a href="../">Sign Up</a> &nbsp;·&nbsp;
-                        <a href="../">Forgot Password</a> &nbsp;·&nbsp;
-                        <a href="../">Need Help?</a>
-                    </p> */}
-                </div>
-            </div>
-        </div>
-    </section>
-    </>
-  )
-}
+                  </div>
 
-export default LoginAdmin
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        className="input is-large"
+                        type="password"
+                        placeholder="Your Password"
+                        onChange={onChangePassword}
+                        value={password}
+                      />
+                    </div>
+                  </div>
+                  <div className="field"></div>
+                  <button
+                    className="button is-block is-warning is-large is-fullwidth is-rounded"
+                    onClick={handleLogin}
+                  >
+                    Login as Admin
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default LoginAdmin;
