65560c831f966e52cef3551e8dbe9470c40260e4
shaki <smsnshakilaa372@gmail.com>
2022-08-12 09:46:20 +0530

    Connected update Student service

---
+++ frontend/src/components/Note.js
@@ -0,0 +1,143 @@
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import axios from "axios";
+
+//Passing the single note to Note component
+const Note = ({ note }) => {
+  const [isModal, setIsModal] = useState(false);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const { user } = useContext(AuthContext);
+  const token = {
+    headers: { token: user.accessToken },
+  };
+
+  const handleClick = () => {
+    setIsModal(!isModal);
+    console.log(isModal);
+  };
+
+  //Declare onChange functions
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const onChangeDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handleUpdate = async () => {
+    const updatedNote = {
+      title: title,
+      description: description,
+    }
+    const res = await axios.put(`http://localhost:5000/api/note/update/${user._id}/${note._id}`, updatedNote, token)
+    .then(()=>{
+        alert("Note updated successfully!");
+        window.location.reload();
+    })
+    .catch((err)=>{
+        alert(err);
+    })
+    console.log(res);
+  };
+
+
+
+  const active = isModal ? "is-active" : "";
+  return (
+    <>
+      <div class="card" style={{ marginBottom: "10px" }}>
+        <header class="card-header">
+          <p class="card-header-title">{note.title}</p>
+          <button class="card-header-icon" aria-label="more options"></button>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            {note.description}
+            <br />
+          </div>
+        </div>
+        <footer class="card-footer">
+          <a href="#" class="card-footer-item" onClick={handleClick}>
+            Edit
+          </a>
+          {/* Delete note */}
+          <a
+            class="card-footer-item"
+            onClick={async () => {
+              console.log(note);
+              await axios.delete(
+                `http://localhost:5000/api/note/delete/${user._id}/${note._id}`,
+                token
+              );
+              window.location.reload();
+              alert("Note deleted successfully!");
+            }}
+          >
+            Delete
+          </a>
+        </footer>
+      </div>
+
+      {/* Update Note Popup */}
+      <div className={`modal ${active}`}>
+        <div className="modal-background" />
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">Edit Note</p>
+            <button
+              onClick={handleClick}
+              className="delete"
+              aria-label="close"
+            />
+          </header>
+          <section className="modal-card-body">
+            <h1 class="title is-1">Edit a Note</h1>
+            <div class="field">
+              <label class="label">Title</label>
+              <div class="control">
+                <input
+                  class="input is-medium"
+                  type="text"
+                  placeholder={note.title}
+                  onChange={onChangeTitle}
+                />
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Description</label>
+              <div class="control">
+                <textarea
+                  class="textarea is-medium"
+                  placeholder={note.description}
+                  onChange={onChangeDescription}
+                ></textarea>
+              </div>
+            </div>
+            <div class="control">
+              <button
+                type="submit"
+                class="button is-link is-fullwidth has-text-weight-medium is-medium mb-3"
+                onClick={handleUpdate}
+              >
+                Update Note
+              </button>
+              <button
+                type="submit"
+                class="button is-link is-fullwidth has-text-weight-medium is-medium"
+                onClick={handleClick}
+              >
+                Cancel
+              </button>
+            </div>
+          </section>
+        </div>
+      </div>
+      {/* Modal */}
+    </>
+  );
+};
+
+export default Note;
--- frontend/src/components/ViewNotesList.js
+++ frontend/src/components/ViewNotesList.js
@@ -1,117 +1,44 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import axios from "axios";
+import Note from "./Note";
 
 const ViewNotesList = () => {
-  
   const [notes, setNotes] = useState([]);
   const [isModal, setIsModal] = useState(false);
-  const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   const token = {
-    headers: {token: user.accessToken},
-  }
+    headers: { token: user.accessToken },
+  };
 
-  const handleClick = () => {
+  const handleClick = (e, note) => {
+    console.log(e);
+    console.log(note);
     setIsModal(!isModal);
     console.log(isModal);
   };
 
   useEffect(() => {
-   const getNotes = async () => {
-    const res = await axios.get(`http://localhost:5000/api/note/view-all-notes/${user._id}`, token);
-    setNotes(res.data.notesOfUser);
-   }
-   getNotes();
+    const getNotes = async () => {
+      const res = await axios.get(
+        `http://localhost:5000/api/note/view-all-notes/${user._id}`,
+        token
+      );
+      setNotes(res.data.notesOfUser);
+    };
+    getNotes();
   }, [user._id]);
 
   console.log(notes);
-  
-  const handleDelete = (noteId) => {
-    console.log("Hello");
-  }
 
   const active = isModal ? "is-active" : "";
 
   return (
-    <div class="column is-two-thirds has-text-left">
+    <div class="column has-text-left">
       <h1 class="title is-1">My Notes</h1>
-      {/* 1st Note  */}
-      {notes?.map((note)=>{
-        return(<div class="card">
-        <header class="card-header">
-          <p class="card-header-title">{note.title}</p>
-          <button class="card-header-icon" aria-label="more options">
-          </button>
-        </header>
-        <div class="card-content">
-          <div class="content">{note.description}
-            <br />
-          </div>
-        </div>
-        <footer class="card-footer">
-          <a href="#" class="card-footer-item" onClick={handleClick}>
-            Edit
-          </a>
-          {/* Delete note */}
-          <a class="card-footer-item" onClick={async () => {
-                  console.log(note);
-                  await axios.delete(
-                    `http://localhost:5000/api/note/delete/${user._id}/${note._id}`,token
-                  );
-                  window.location.reload();
-                  alert("Note deleted successfully!");
-                }}>
-            Delete
-          </a>
-        </footer>
-      </div>)
+      {notes?.map((note) => {
+        return <Note note={note} />;
       })}
-
-      {/* Modal */}
-      <div className={`modal ${active}`}>
-        <div className="modal-background" />
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <p className="modal-card-title">Modal title</p>
-            <button
-              onClick={handleClick}
-              className="delete"
-              aria-label="close"
-            />
-          </header>
-          <section className="modal-card-body">
-            <h1 class="title is-1">Edit a Note</h1>
-            <div class="field">
-              <label class="label">Title</label>
-              <div class="control">
-                <input class="input is-medium" type="text" />
-              </div>
-            </div>
-            <div class="field">
-              <label class="label">Description</label>
-              <div class="control">
-                <textarea class="textarea is-medium"></textarea>
-              </div>
-            </div>
-            <div class="control">
-              <button
-                type="submit"
-                class="button is-link is-fullwidth has-text-weight-medium is-medium mb-3"
-              >
-                Create Note
-              </button>
-              <button
-                type="submit"
-                class="button is-link is-fullwidth has-text-weight-medium is-medium"
-                onClick={handleClick}
-              >
-                Cancel
-              </button>
-            </div>
-          </section>
-        </div>
-      </div>
-      {/* Modal */}
     </div>
   );
 };
